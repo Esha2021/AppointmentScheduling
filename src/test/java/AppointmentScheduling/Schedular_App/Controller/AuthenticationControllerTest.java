3758220bebd437323bc8d15151bd1fc5d8624a8a
Esha2021 <eeswaricse@gmail.com>
2026-08-15 18:56:34 -0500

    Fix JwtUtil references

--- src/test/java/AppointmentScheduling/Schedular_App/Controller/AuthenticationControllerTest.java
+++ src/test/java/AppointmentScheduling/Schedular_App/Controller/AuthenticationControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
